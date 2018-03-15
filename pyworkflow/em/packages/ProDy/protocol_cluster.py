02172959b95d128c6ff1a5896ae3396f5dcaf387
Amaya Jimenez <ajimoreno@gmail.com>
2018-03-15 20:48:38 +0100

    added clustering protocol

---
+++ pyworkflow/em/packages/ProDy/protocol_cluster.py
@@ -0,0 +1,118 @@
+# **************************************************************************
+# *
+# * Authors:  Javier Mota Garcia (jmota@cnb.csic.es), February 2018
+# *
+# * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 2 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'scipion@cnb.csic.es'
+# *
+# **************************************************************************
+
+from pyworkflow.em import *
+import pyworkflow.protocol.constants as pwconst
+from prody import *
+from pyworkflow.utils import *
+import time
+from Bio import Phylo
+
+class clusterPdbTrajectories(EMProtocol):
+    """ Protocol to execute functions from ProDy software"""
+
+    _label = "Cluster Trajectories ProDy"
+
+    def _defineParams(self, form):
+        self.defaultCycles = 5
+
+        form.addSection(label="ProDy Cluster Trajectories")
+        form.addParam('setOfTrajectories', PointerParam,
+                      pointerClass='SetOfTrajectories',
+                      label="Set of Trajectories",
+                      important=True)
+        form.addParam('distanceMatrixFile', PointerParam,
+                      pointerClass='EMFile',
+                      label="Distance Matrix File",
+                      important=True)
+        form.addParam('subgroupCutoff', params.FloatParam, default=0.01,
+                      label='Distance cutoff for defining clusters',
+                      expertLevel=pwconst.LEVEL_ADVANCED,
+                      help='The distance in PCA space that is used for '
+                           'dividing the tree into subgroups for hierarchical '
+                           'clustering.')
+        form.addParam('treeMethod', params.EnumParam, choices=['nj', 'upgma'],
+                      expertLevel=pwconst.LEVEL_ADVANCED,
+                      default=0,
+                      display=params.EnumParam.DISPLAY_HLIST,
+                      label="Method to use for tree construction")
+
+    def _insertAllSteps(self):
+        self._insertFunctionStep('clusterTrajectories')
+        self._insertFunctionStep('createOutputStep')
+
+    def clusterTrajectories(self):
+
+        if self.treeMethod.get() == 0:
+            treeMethod = 'nj'
+        else:
+            treeMethod = 'upgma'
+
+        #import time
+        #time.sleep(10)
+
+        for trajectory in self.setOfTrajectories.get():
+            fileName = trajectory.getFileName()
+            break
+
+        ens = parseDCD(str(fileName))
+        pdb = parsePDB(str(trajectory._initialPdb))
+        ca = pdb.ca.copy()
+        ens.setCoords(ca)
+        ens.setAtoms(ca)
+
+        fnPdb = []
+        setOfPDBs = self._createSetOfPDBs()
+        numbers = []
+        for i, conformation in enumerate(ens):
+            fnPdb.append(self._getExtraPath('pdb{:02d}.pdb'.format(i)))
+            writePDB(fnPdb[i], conformation)
+            pdb = PdbFile(fnPdb[i])
+            setOfPDBs.append(pdb)
+            numbers.append(str(i))
+
+        distanceMatrix = parseArray(str(self.distanceMatrixFile.get(
+        ).getFileName()))
+
+        self.tree = calcTree(names=numbers, distance_matrix=distanceMatrix,
+                             method=treeMethod)
+        self.subgroups = findSubgroups(self.tree, self.subgroupCutoff.get())
+
+        self.setOfRepresentatives = self._createSetOfPDBs()
+        for subgroup in self.subgroups:
+            print subgroup
+            memberName = subgroup[0]
+            Pdb = PdbFile(self._getExtraPath('pdb{:02d}.pdb'.format(
+                int(memberName))))
+            self.setOfRepresentatives.append(Pdb)
+
+    def createOutputStep(self):
+        Phylo.write(self.tree, self._getExtraPath('clustering_tree.nwk'),
+        'newick')
+        myFile = EMFile(self._getExtraPath('clustering_tree.nwk'))
+        self._defineOutputs(ClusteringTree=myFile)
+
+        self._defineOutputs(representivePDBs=self.setOfRepresentatives)
---
+++ pyworkflow/em/packages/ProDy/viewerCluster.py
@@ -0,0 +1,102 @@
+# **************************************************************************
+# *
+# * Authors:     Javier Mota Garcia (jmota@cnb.csic.es)
+# *
+# * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 2 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'scipion@cnb.csic.es'
+# *
+# **************************************************************************
+"""
+This module implement the wrappers around ProDy protocol
+visualization program.
+"""
+
+from pyworkflow.viewer import ProtocolViewer, DESKTOP_TKINTER, WEB_DJANGO
+from pyworkflow.em import *
+from computePCATrajectory import computeModesPcaPdb
+from prody import *
+import matplotlib.pyplot as plt
+
+
+class ProdyViewerPca(Viewer):
+    """ Wrapper to visualize Pdb to SAXS. """
+    _targets = [computeModesPcaPdb]
+    _environments = [DESKTOP_TKINTER, WEB_DJANGO]
+
+    def __init__(self, **args):
+        Viewer.__init__(self, **args)
+
+    def visualize(self, obj, **args):
+
+        cls = type(obj)
+
+        if issubclass(cls, computeModesPcaPdb):
+            fnPca = obj._getExtraPath("pcaModes.pca.npz")
+            setOfTraj = obj.setOfTrajectories.get()
+
+            for i, traj in enumerate(setOfTraj):
+                Pdb = parsePDB(str(traj._initialPdb))
+                break
+
+            protein = Pdb.select('protein and not hydrogen').copy()
+
+            pca = loadModel(fnPca)
+
+            combinedTrajSet = obj.combinedTrajectory
+            for combinedTraj in combinedTrajSet:
+                combinedEns = parseDCD(str(combinedTraj.getFileName()))
+
+            combinedEns.setCoords(protein.ca.copy())
+            combinedEns.setAtoms(protein.ca)
+
+            colors = []
+            for i, traj in enumerate(obj.setOfTrajectories.get()):
+
+                c = (random.random(), random.random(), random.random())
+
+                ens = parseDCD(str(traj._filename))
+                ens.setCoords(protein)
+                ens.setAtoms(protein.ca)
+
+                for j in range(len(ens)):
+                    if j == 0:
+                        colors.append((1,0,0))
+                    else:
+                        colors.append(c)
+
+            show, fig, ax = showProjection(combinedEns, pca[:2],
+                                           new_fig=True,
+                                           color=colors, markeredgewidth=0,
+                                           returnAx=True)
+
+            projection = calcProjection(combinedEns, pca[:2])
+            for n, point in enumerate(projection):
+                ax.annotate(str(n), (point[0], point[1]))
+
+            distanceMatrix = parseArray(str(obj.distanceMatrix
+                                            .getFileName()),'\t')
+            plt.figure()
+            showMatrix(distanceMatrix)
+            plt.show()
+
+
+
+
+
