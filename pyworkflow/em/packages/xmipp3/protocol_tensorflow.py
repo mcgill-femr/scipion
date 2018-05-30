d3334563621a99f1e1329ef733d5704a790a424f
JaviMota <jamotga26@gmail.com>
2018-05-30 10:39:32 +0200

    tensorflow tests

--- pyworkflow/em/packages/xmipp3/protocol_tensorflow.py
+++ pyworkflow/em/packages/xmipp3/protocol_tensorflow.py
@@ -27,8 +27,8 @@
 import numpy as np
 import keras
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten, BatchNormalization
-from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Dense, Dropout, Flatten, BatchNormalization, Activation
+from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D
 from keras.optimizers import SGD
 from keras.datasets import cifar10
 import matplotlib
@@ -46,41 +46,57 @@ y_test = keras.utils.to_categorical(np.random.randint(10, size=(20, 1)), num_cla
 
 y_train = keras.utils.to_categorical(y_train, num_classes)
 y_test = keras.utils.to_categorical(y_test, num_classes)
-
 model = Sequential()
 # input: 100x100 images with 3 channels -> (100, 100, 3) tensors.
 # this applies 32 convolution filters of size 3x3 each.
-model.add(Conv2D(32, (3, 3), activation='relu', input_shape=x_train.shape[1:]))
+model.add(Conv2D(32, (5, 5),padding='same',input_shape=x_train.shape[1:]))
 model.add(BatchNormalization())
-model.add(Conv2D(32, (3, 3), activation='relu'))
+#model.add(MaxPooling2D(pool_size=(3, 3)))
+model.add(Dropout(0.1))
+model.add(ZeroPadding2D(padding=(5,5)))
+model.add(Conv2D(32, (5, 5)))
 model.add(BatchNormalization())
-model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Activation('leakyrelu'))
+model.add(MaxPooling2D(pool_size=(3, 3)))
 model.add(Dropout(0.25))
 
-model.add(Conv2D(64, (5, 5), activation='relu'))
+model.add(ZeroPadding2D(padding=(3,3)))
+model.add(Conv2D(64, (3, 3)))
 model.add(BatchNormalization())
-model.add(Conv2D(64, (5, 5), activation='relu'))
+model.add(Dropout(0.1))
+#model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(ZeroPadding2D(padding=(3,3)))
+model.add(Conv2D(64, (3, 3)))
 model.add(BatchNormalization())
+model.add(Activation('relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
 
 model.add(Flatten())
 model.add(Dense(256, activation='relu'))
-#model.add(BatchNormalization())
+model.add(BatchNormalization())
 model.add(Dropout(0.5))
 model.add(Dense(10, activation='softmax'))
 
 #sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-history = model.fit(x_train, y_train, validation_split=0.2, batch_size=32, epochs=35)
+history = model.fit(x_train, y_train, validation_split=0.2, batch_size=32, epochs=50)
 print history.history.keys()
 score = model.evaluate(x_test, y_test, batch_size=32)
 print score
-prediction = model.predict_classes(x_test, batch_size=32)
-print prediction
+'''prediction = model.predict_classes(x_test, batch_size=32)
+ok = 0
+for i, x in enumerate(prediction):
+    if x == np.where(y_test[i]==np.max(y_test[i]))[0]:
+        ok += 1
+
+accuracy = ok/float(len(y_test))
+print accuracy'''
 plt.figure()
 plt.plot(history.history['loss'])
 plt.figure()
 plt.plot(history.history['acc'])
+plt.figure()
+plt.plot(history.history['val_acc'])
 plt.show()
