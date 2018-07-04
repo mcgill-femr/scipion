7b0f62c598900f94c486f00d4203c7e2194bbd9e
JaviMota <jamotga26@gmail.com>
2018-07-04 12:13:17 +0200

    tensorflow practices

--- pyworkflow/em/packages/xmipp3/protocol_tensorflow.py
+++ pyworkflow/em/packages/xmipp3/protocol_tensorflow.py
@@ -27,10 +27,11 @@
 import numpy as np
 import keras
 import tensorflow as tf
+from keras import backend as K
 from keras.preprocessing.image import ImageDataGenerator
-from keras.models import Sequential, Model
-from keras.layers import Dense, Dropout, Flatten, BatchNormalization, Activation
-from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D, Input, UpSampling2D
+from keras.models import Sequential, Model, load_model
+from keras.layers import Dense, Dropout, Flatten, BatchNormalization, Activation, LeakyReLU
+from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D, Input, UpSampling2D, Lambda, Reshape, AveragePooling2D
 from keras.optimizers import SGD
 from keras.callbacks import TensorBoard
 from keras.datasets import cifar10, mnist
@@ -42,9 +43,12 @@ import mrcfile
 import glob
 from keras.applications import VGG19
 import cv2
+import shutil
+import os
 
+#shutil.rmtree('/tmp/tb')
 # dimensions of our images.
-img_width, img_height = 100, 100
+size = 75
 
 train_data_dir = '/home/javiermota/Downloads/images/Train/cats/*.jpg'
 validation_data_dir = '/home/javiermota/images_tensorflow/Validation'
@@ -52,61 +56,132 @@ test_data_dir = '/home/javiermota/images_tensorflow/Test'
 nb_train_samples = 30
 nb_validation_samples = 18
 epochs = 50
-batch_size = 8
+batch_size = 32
 
 (x_train, _), (x_test, _) = mnist.load_data()
+
+imagesTrain_resized = []
+imagesTest_resized = []
+im_resi = []
+for im in x_train:
+    im_resize = cv2.resize(im,(size,size))
+    imagesTrain_resized.append(im_resize)
+for im in x_test:
+    im_resizeTest = cv2.resize(im,(size,size))
+    imagesTest_resized.append(im_resizeTest)
+x_train = np.asarray(imagesTrain_resized)
+x_test = np.asarray(imagesTest_resized)
 x_train = x_train.astype('float32') / 255.
 x_test = x_test.astype('float32') / 255.
-x_train = x_train.reshape((len(x_train), 28,28,1))
-x_test = x_test.reshape((len(x_test), 28,28,1))
+x_train = x_train.reshape((len(x_train), size,size,1))
+x_test = x_test.reshape((len(x_test), size,size,1))
 print x_train.shape
 print x_test.shape
 
-noise_factor = 0.5
+noise_factor = 2
 x_train_noisy = x_train + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_train.shape)
 x_test_noisy = x_test + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_test.shape)
 
 x_train_noisy = np.clip(x_train_noisy, 0., 1.)
 x_test_noisy = np.clip(x_test_noisy, 0., 1.)
+plt.imshow(x_train_noisy[0].reshape(size,size))
+plt.gray()
+plt.show()
+
+paths = []
+for image in glob.glob('/home/javiermota/Downloads/archive/10184/data/*.mrc'):
+    paths.append(image)
+
+def data_gen(paths):
+    while True:
+        batch_img = []
+        labels = []
+        for i in range(batch_size):
+            k = np.random.randint(0,len(paths))
+            with mrcfile.open(paths[k]) as mrc:
+                image = cv2.resize(mrc.data,(size,size))
+                plt.figure()
+                plt.imshow(image)
+                plt.gray()
+                plt.show()
+                image = np.reshape(image,(size,size,1))
+                batch_img.append(image)
+                labels.append(image)
+
+        #yield np.asarray(batch_img).astype('float32')/255., np.asarray(labels)
+
+model = load_model('/home/javiermota/autoencoder.h5')
+
+for layers in model.layers[:-1]:
+    layers.trainable = False
+    print layers, layers.trainable
+
+micrographs = Sequential()
+micrographs.add(model)
+micrographs.compile(optimizer='adam',loss='binary_crossentropy')
+micrographs.fit_generator(data_gen(paths),steps_per_epoch=50, epochs=10,
+                          callbacks=[TensorBoard(log_dir='/tmp/tb', histogram_freq=1, write_graph=False, write_images=True)])
+
+
+
+
 
 '''noisyImage = []
 for im in glob.glob(train_data_dir):
     image = cv2.imread(im,0)
+    image.astype('float32')/255.
     noise = np.random.normal(loc=0.0, scale=1, size=np.shape(image))
     noiseimage = cv2.resize(image+noise,(128,128))
     noisyImage.append(np.ravel(noiseimage))
     #plt.imshow(noiseimage,cmap = plt.get_cmap('gray'))
 
 noisyImage = np.asarray(noisyImage)
-noisyImage.astype('float32')/255.
 noisyImage = np.reshape(noisyImage,(len(noisyImage),128,128,1))'''
 
-input_img = Input(shape=(28,28,1))
 
-x = Conv2D(32, (3, 3), activation='relu', padding='same')(input_img)
-x = MaxPooling2D((2, 2), padding='same')(x)
-x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
-encoded = MaxPooling2D((2, 2), padding='same')(x)
+input_img = Input(shape=(size,size,1),name='input')
+
+x = Conv2D(32, (15, 15), activation='relu', padding='same')(input_img)
+x = MaxPooling2D((5, 5), padding='same')(x)
+x = BatchNormalization()(x)
+x = Conv2D(32, (7, 7), activation='relu', padding='same')(x)
+encoded = MaxPooling2D((3, 3), padding='same', name='encoded')(x)
+encoded = BatchNormalization()(encoded)
 
 # at this point the representation is (7, 7, 32)
 
-x = Conv2D(32, (3, 3), activation='relu', padding='same')(encoded)
-x = UpSampling2D((2, 2))(x)
-x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
-x = UpSampling2D((2, 2))(x)
-decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
+x = Conv2D(32, (7, 7), activation='relu', padding='same')(encoded)
+x = UpSampling2D((3, 3))(x)
+x = BatchNormalization()(x)
+x = Conv2D(32, (15, 15), activation='relu', padding='same')(x)
+x = UpSampling2D((5, 5))(x)
+x = BatchNormalization()(x)
+decoded = Conv2D(1, (15, 15), activation='sigmoid', padding='same', name='decoded')(x)
+
+'''x = Dense(32,activation='relu')(input_img)
+x = Dense(16,activation='relu')(x)
+x = Dense(8,activation='relu', name='encoded')(x)
+x = Dense(16,activation='relu')(x)
+x = Dense(32,activation='relu')(x)
+decoded = Dense(size,activation='sigmoid', name='decoded')(x)'''
+
 
 autoencoder = Model(input_img, decoded)
-image = tf.reshape(autoencoder[:1],[-1,28,28,1])
-tf.summary.image("image", image)
-autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
 
+autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+tf.summary.image("image",autoencoder.get_layer('decoded').output)
+'''for i in range(0,31):
+    tf.summary.image("image"+str(i),K.expand_dims(autoencoder.get_layer('decoded').output[:,:,:,i]))'''
+#autoencoder.fit_generator(data_gen(paths),steps_per_epoch=100,nb_epoch=50)
+encoded_layer = Model(inputs=autoencoder.input,outputs=autoencoder.get_layer('encoded').output)
+#autoencoder.save_weights('/home/javiermota/weights_improved.h5')
+autoencoder.save('/home/javiermota/autoencoder.h5')
 autoencoder.fit(x_train_noisy, x_train,
-                epochs=100,
-                batch_size=128,
+                epochs=10,
+                batch_size=32,
                 shuffle=True,
                 validation_data=(x_test_noisy, x_test),
-                callbacks=[TensorBoard(log_dir='/tmp/tb', histogram_freq=0, write_graph=False, write_images=True)])
+                callbacks=[TensorBoard(log_dir='/tmp/tb', histogram_freq=1, write_graph=False, write_images=True)])
 
 #train = ImageDataGenerator().flow_from_directory(train_data_dir, target_size=(img_width,img_height),  classes=['views'], batch_size=batch_size)
 #validation = ImageDataGenerator().flow_from_directory(validation_data_dir, target_size=(img_width,img_height), classes=['views'], batch_size=batch_size/2)
